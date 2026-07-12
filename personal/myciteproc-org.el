51f77fee9c485809df73b497a5a07ec6d0c78ff9
Joseph Vidal-Rosset <joseph@vidal-rosset.net>
2026-07-12 15:41:15 +0200

    Clear outdated 2021 content before full rewrite

--- personal/myciteproc-org.el
+++
@@ -1,7 +0,0 @@
-;;; myciteproc-org.el ---                            -*- lexical-binding: t; -*-
-
-;; Copyright (C) 2021  Joseph VIdal-Rosset
-
-;; Author: Joseph VIdal-Rosset <joseph@vidal-rosset.net>
-;; Keywords: convenience, abbrev,
-(citeproc-org-setup)
--- personal/myorgrefile.el
+++
@@ -1,7 +0,0 @@
-;;; myorgrefile.el --- to move a heading level one to a file.   -*- lexical-binding: t; -*-
-
-;; Copyright (C) 2021  Joseph Vidal-Rosset
-
-;; Author: Joseph Vidal-Rosset <joseph@vidal-rosset.net>
-;; Keywords: convenience,
-(setq org-refile-use-outline-path 'file)
